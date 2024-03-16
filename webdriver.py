885acca6868e4cd679e3198b832f9c6568691c28
Xavier <collantes.xavier@gmail.com>
2024-03-15 23:34:36 -0700

    Add main
    Signed-off-by: Xavier <collantes.xavier@gmail.com>

--- webdriver.py
+++ webdriver.py
@@ -1,7 +1,30 @@
+"""Run script."""
+
+import time
+import logging
 from secure_selenium.secure_selenium import SecureSelenium
 
+logging.basicConfig(level=logging.INFO)
 
-driver = SecureSelenium(webdriver_path="./secure_selenium/chromedriver", headless=False)
 
-driver.get("https://google.com")
-driver.save_screenshot()
+def main():
+    driver = SecureSelenium(
+        webdriver_path="/home/xavier/Documents/instagram_automation/chromedriver-linux64/chromedriver",
+        headless=False,
+    )
+
+    driver.initial_cookies()
+
+    try:
+        driver.get("https://www.instagram.com/")
+
+        logging.info("Sleeping... zzz")
+        time.sleep(5)
+    except KeyboardInterrupt as ki:
+        logging.warning("Stopped by keyboard: %s", ki)
+    finally:
+        driver.close()
+
+
+if __name__ == "__main__":
+    main()
